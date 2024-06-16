c82aee67a4add0fccd92c96250c7a9f33f909b2a
Tamir Abutbul <1tamir198@gmail.com>
2024-06-16 20:38:21 +0300

    Adding support for exponent operator

--- service/DefaultService.js
+++ service/DefaultService.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 /**
  * Perform arithmetic operation
  * Receives two numeric values and performs an arithmetic operation specified in the header.
@@ -9,17 +8,36 @@
  * operation String The arithmetic operation to perform
  * returns inline_response_200
  **/
-exports.calculatePOST = function(body,operation) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-    "result" : 0.8008281904610115
-};
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
-}
+exports.calculatePOST = function (body, operation) {
+  return new Promise(function (resolve, reject) {
+    const { firstNum, secondNum } = body;
+    let result;
 
+    switch (operation) {
+      case 'add':
+        result = firstNum + secondNum;
+        break;
+      case 'substract':
+        result = firstNum - secondNum;
+        break;
+      case 'multiply':
+        result = firstNum * secondNum;
+        break;
+      case 'divide':
+        if (secondNum === 0) {
+          reject({ error: 'Division by zero is not allowed' });
+          return;
+        }
+        result = firstNum / secondNum;
+        break;
+      case 'exponent':
+        result = Math.pow(firstNum, secondNum);
+        break;
+      default:
+        reject({ error: 'Invalid or not supported operation' });
+        return;
+    }
+
+    resolve({ result });
+  });
+};
